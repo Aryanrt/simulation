f40127874cd483b9b8d2472e4995484b5c464991
Aryan <aryan.r.t@gmail.com>
2022-03-05 19:14:58 -0500

    some cleanup

--- modeling.py
+++ modeling.py
@@ -57,15 +57,14 @@ def generateRandomNumbers():
     return digits;
 
 def plotQQExp(data,name):
-    # dist = ot.Exponential(1/calulateWeightedMean(data));
-    # x = dist.getSample(300);
-    # x = np.random.exponential(calulateWeightedMean(data),300);
+
     generatedSample= [0]*300;
     landa= 1/np.mean(data);
     randomNums = generateRandomNumbers();
     for i in range(0,300):
         generatedSample[i] = (-1/landa)* np.log(1-randomNums[i]);
-    x= generatedSample;
+
+    # get min and max
     maxSample=0;
     for i in range(0,300):
         if maxSample < generatedSample[i]:
@@ -77,10 +76,6 @@ def plotQQExp(data,name):
         if minSample > generatedSample[i]:
             minSample= generatedSample[i];
             index = i;
-    width = (max(max(data), maxSample)-(min(min(data), minSample))) /18;
-    # widths = [0]*18;
-    # sampleBin = [0] * 18;
-    # distBin = [0] * 18;
 
     if name=="servinsp1" or name=="servinsp22":
         size =9;
@@ -115,18 +110,24 @@ def plotQQExp(data,name):
     plt.savefig(name+'-QQ-EXP.png');
     return str(x2);
 
+# --------------------------------------------------------------------------------------------------------
 def plotQQGeom(data,name):
 
-    x = np.random.geometric(1/calulateWeightedMean(data),300);
+    generatedSample= [0]*300;
+    p= 1/np.mean(data);
+    randomNums = generateRandomNumbers();
+    for i in range(0,300):
+        generatedSample[i] = np.log(1-randomNums[i])/ np.log(1-p);
+    
     maxSample=0;
     for i in range(0,300):
-        if maxSample < x[i]:
-            maxSample= x[i];
+        if maxSample < generatedSample[i]:
+            maxSample= generatedSample[i];
     
     minSample= maxSample;
     for i in range(0,300):
-        if minSample > x[i]:
-            minSample= x[i];
+        if minSample > generatedSample[i]:
+            minSample= generatedSample[i];
 
     if name=="servinsp1" or name=="servinsp22":
         size =9;
@@ -146,7 +147,7 @@ def plotQQGeom(data,name):
     
     for i in range (0,size):
         for j in range(0,300):
-            if x[j] >= i * width + minData and x[j]< (i+1) * width + minData:
+            if generatedSample[j] >= i * width + minData and generatedSample[j]< (i+1) * width + minData:
                 distBin[i] = distBin[i] + 1;
             if data[j] >= i * width + minData and data[j] < (i+1) * width + minData:
                 sampleBin[i] = sampleBin[i] + 1;
@@ -154,47 +155,12 @@ def plotQQGeom(data,name):
     sampleBin[size-1] = 300 - sum(sampleBin);
     
     x2 = calculateX2(sampleBin, distBin,size);
-    # plt.clf();
-    # plt.plot(sampleBin, distBin)
-    # xpoints = ypoints = plt.xlim()
-    # plt.plot(xpoints, ypoints, linestyle='--', color='k', lw=3, scalex=False, scaley=False)
-    # plt.title(name+'-QQ-Geometric \n with x2= '+str(x2));
-    # plt.savefig(name+'-QQ-GEO.png')
-    return str(x2);
-
-def plotQQPoisson(data,name):
-    dist = ot.Poisson(calulateWeightedMean(data));
-    x = dist.getSample(300);
-    maxSample=0;
-    for i in range(0,300):
-        if maxSample < x[i][0]:
-            maxSample= x[i][0];
-    minSample= maxSample;
-    for i in range(0,300):
-        if minSample > x[i][0]:
-            minSample= x[i][0];
-    width = (max(max(data), maxSample)-(min(min(data), minSample))) /18;
-    
-    sampleBin = [0] * 18;
-    distBin = [0] * 18;
-    for i in range (0,18):
-        for j in range(0,300):
-            if x[j][0] >= i * width and x[j][0] < (i+1) * width:
-                distBin[i] = distBin[i] + 1;
-            if data[j] >= i * width and data[j] < (i+1) * width:
-                sampleBin[i] = sampleBin[i] + 1;
-    
-    # Case of samples equal to the maximum of both samples(which is not taken care of above)
-    distBin[16] = distBin[16] + (300-sum(distBin));
-    sampleBin[16] = sampleBin[16] + (300-sum(sampleBin));
-
-    x2 = calculateX2(sampleBin, distBin,18);
     plt.clf();
     plt.plot(sampleBin, distBin)
     xpoints = ypoints = plt.xlim()
     plt.plot(xpoints, ypoints, linestyle='--', color='k', lw=3, scalex=False, scaley=False)
-    plt.title(name+'-QQ-POISSON \n with x2= '+str(x2));
-    plt.savefig(name+'-QQ-POISSON.png')
+    plt.title(name+'-QQ-Geometric \n with x2= '+str(x2));
+    plt.savefig(name+'-QQ-GEO.png')
     return str(x2);
 
 
@@ -218,14 +184,6 @@ plotHistograms(ws1,'ws1');
 plotHistograms(ws2,'ws2');
 plotHistograms(ws3,'ws3');
 
-# calulateWeightedMean(servinsp1,'servinsp1');
-# calulateWeightedMean(servinsp22,'servinsp22');
-# calulateWeightedMean(servinsp23,'servinsp23');
-# calulateWeightedMean(ws1,'ws1');
-# calulateWeightedMean(ws2,'ws2');
-# calulateWeightedMean(ws3,'ws3');
-
-
 # QQ for Expnential
 as1 = plotQQExp(servinsp1,'servinsp1');
 as22 = plotQQExp(servinsp22,'servinsp22');
@@ -242,19 +200,20 @@ bw1 = plotQQGeom(ws1,'ws1');
 bw2 = plotQQGeom(ws2,'ws2');
 bw3 = plotQQGeom(ws2,'ws3');
 
-# QQ for Possion
-cs1 =plotQQPoisson(servinsp1,'servinsp1');
-cs22 = plotQQPoisson(servinsp22,'servinsp22');
-cs23 = plotQQPoisson(servinsp23,'servinsp23');
-cw1 = plotQQPoisson(ws1,'ws1');
-cw2 = plotQQPoisson(ws2,'ws2');
-cw3 = plotQQPoisson(ws3,'ws3');
 
-
-print('------------------');
-print(as1 + ' '+ bs1+ ' '+ cs1);
-print(as22 + ' '+ bs22+ ' '+ cs22);
-print(as23 + ' '+ bs23+ ' '+ cs23);
-print(aw1 + ' '+ bw1+ ' '+ cw1);
-print(aw2 + ' '+ bw2+ ' '+ cw2);
-print(aw3 + ' '+ bw3+ ' '+ cw3);
+print('Exponential Chi Square Restlts:');
+print('\nins1\t'+as1);
+print('ins22\t'+as22);
+print('ins23\t'+as23);
+print('ws1\t'+aw1);
+print('ws2\t'+aw2);
+print('ws3\t'+aw3);
+print('\n------------------');
+print('Geomteric Chi Square Restlts:');
+print('\nins1\t'+bs1);
+print('ins22\t'+bs22);
+print('ins23\t'+bs23);
+print('ws1\t'+bw1);
+print('ws2\t'+bw2);
+print('ws3\t'+bw3);
+print('\n------------------');
